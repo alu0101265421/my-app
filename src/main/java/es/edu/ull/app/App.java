21c9ccc70e3532a3f3dba6e3e62adf1c5585af04
Elena <alu0101265421@ull.edu.es>
2021-11-19 11:55:19 +0000

    Initial commit

---
+++ src/main/java/es/edu/ull/app/App.java
@@ -0,0 +1,14 @@
+package es.edu.ull.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	System.out.println( "Estamos en la asignatura LDH" );
+    }
+}
